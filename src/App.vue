27d6441e2447638c5d6b23cd973bc4bc00887cc9
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2023-04-16 18:40:11 +0700

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
+  <h1> Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <BlockGame v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score"/>
+  </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import BlockGame from './components/Block.vue';
+import Result from './components/Result.vue';
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    BlockGame,Result
   },
+  data() {
+    return {
+     isPlaying: false,
+     delay: null,
+     score: null,
+     showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.showResult = false;
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log("the game started after " + this.delay + "ms");
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      console.log("the game ended after " + reactionTime + "ms");
+      this.showResult = true;
+    }
+  }
 };
 </script>
 
---
+++ src/components/Result.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Result</h1>
+        <p>your score is {{ score }}ms</p>
+        <p>your rank is {{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            rank: null
+        }
+    },
+    props: ['score']
+    , mounted() {
+        if (this.score < 250) {
+            this.rank = 'Master'
+        } else if (this.score < 500) {
+            this.rank = 'Expert'
+        } else {
+            this.rank = 'Newbie'
+        }
+    }
+}
+
+</script>
